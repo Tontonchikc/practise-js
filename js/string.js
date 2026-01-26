403cd7248460fb0148203e5966553bb2c59f090f
Tontonchikc <pranganton@gmail.com>
2026-01-26 19:29:24 +0200

    string

---
+++ js/string.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const message =  'Мій перший скрипт';
+console.log(message);
+
+const firstLetter = message[0];
+console.log(firstLetter);
+
+const messageLength = message.length;
+console.log(messageLength);
+// const lastLetter = message[16]; //
+const lastLetter = message[message.length - 1];// використовуємо це,тому що він ав томатично знаходить останій символ
+console.log(lastLetter);
+
+const greetings = 
