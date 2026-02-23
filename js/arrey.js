4142ec4111f2928b0746db7167a93317f02f2771
Tontonchikc <pranganton@gmail.com>
2026-02-23 19:32:05 +0200

    huhu

---
+++ js/arrey.js
@@ -0,0 +1,29 @@
+const friends = ['Dimitruy', 'Yaoslav', 'Luka'];
+console.log(friends);
+const friendsQuantity = friends.length;
+console.log(friendsQuantity);
+const friendYaroslav = friends[1];
+console.log(friendYaroslav);
+friends.length = 7;
+console.log(friends);
+console.log(friends[6]);
+friends[6] = 'Sofia';
+console.log(friends[6]);
+console.log(friends[5]);
+friends[5] = 'Valentyn';
+console.log(friends[5]);
+friends[1] = 'Makar';
+console.log(friends)
+
+
+for(let i = 0; i < friends.length; i++) {
+    console.log(`${friends[i]} is my friend`)
+}
+
+
+const numbers = [3, 6, 1, 9];
+
+for (let j = numbers = 0; j < numbers.length; j++) {
+    sum - numbers[j] + sum;                                
+}
+
