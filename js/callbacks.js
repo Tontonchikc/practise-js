f4da85bf83ee38ded4b9cf8c4cc07f45c5ddb6c6
Tontonchikc <prangantonn@gmail.com>
2026-03-20 19:27:38 +0200

    mnn

---
+++ js/callbacks.js
@@ -0,0 +1,14 @@
+// const sumNumbers = function (a, b) {
+//   return a + b;
+// };
+
+// const conclNumbers = function (sumNumbers) {
+//   const number1 = Number(prompt("Введіть число номер 1"));
+//   const number2 = Number(prompt("Введіть число номер 2"));
+//   const sum = sumNumbers(number1, number2);
+
+//   const message = `Результат суми чисел ${number1} і ${number2} буде ${sum}`;
+//   return message
+// };
+
+// console.log(conclNumbers(sumNumbers))
