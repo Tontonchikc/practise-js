0fc986f8da893bab8adaa6b36b09250cddfdb037
Tontonchikc <prangantonn@gmail.com>
2026-05-01 19:33:10 +0300

    oop

--- js/oop.js
+++ js/oop.js
@@ -1,10 +1,15 @@
 class Characters {
-    #maxHealth = 500;
-    #greeting 
+  static characterCount = 0;
+  static allCharacters = [];
+
+  #maxHealth = 500;
+  #greeting;
   constructor({ name, health }) {
     this.name = name;
     this.health = health;
     this.#greeting = "Hello there";
+    Characters.charactersCount++;
+    Characters.allCharacters.push(this);
   }
 
   isMove(x, y) {
@@ -32,12 +37,26 @@ class Characters {
     }
     this.#greeting = newGreeting;
   }
+
+  static getAlive() {
+    return Characters.allCharacters.filter((character) => character.health > 0);
+  }
+
+  get maxHealth() {
+    return this.#maxHealth;
+  }
+
+  static getDamagedHealth() {
+    return Characters.allCharacters.filter(
+      (character) => character.health !== character.maxHealth,
+    );
+  }
 }
 
 const man = new Characters({ name: "I", health: 100 });
 console.log(man);
 
-const piter = new Characters({ health: 101, name: "Piter Parker" });
+const piter = new Characters({ health: 0, name: "Piter Parker" });
 console.log(piter);
 
 const person = new Characters({ health: 800, name: "Anton" });
@@ -48,4 +67,31 @@ man.sayHello();
 man.greeting = "Hello world";
 man.sayHello();
 
-console.log(person.isMove(5, 20));
+person.isMove(5, 20);
+
+console.log(Characters.charactersCount);
+
+console.log(Characters.getAlive());
+console.log(Characters.getDamagedHealth());
+
+
+class mage extends Characters {
+  constructor({ name, health, energy }) {
+    super({ name, health });
+    this.energy = energy;
+  }
+  magesAttack(hero) {
+    if (this.energy < 50){ console.error("Low energy")
+      return;
+    }
+    this.energy -= 50
+    hero.health -= 100
+    
+  }
+  
+
+}
+
+
+const mage = new mage({ name: "Mars", health: 500, energy: 400 });
+console.log(mage)
