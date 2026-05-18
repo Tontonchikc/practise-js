6a87fb9e848e2fe5f532deafcc54886d4d1cc549
Tontonchikc <prangantonn@gmail.com>
2026-05-18 18:16:34 +0300

    67

--- js/dom2.js
+++ js/dom2.js
@@ -34,3 +34,108 @@ colorPickerOptions.forEach(element => {
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const container = document.querySelector("[data-container]");
+const text = document.createElement("p");
+
+text.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+
+text.classList.add("text");
+
+container.before(text);
+
+const input = document.createElement("input");
+
+// input.type = "text";
+
+input.placeholder = "Please, input the number";
+
+input.setAttribute("type", "number");
+
+console.log(input.hasAttribute("placeholder"));
+
+console.log(input.getAttribute("type"));
+
+input.removeAttribute("placeholder");
+
+console.log(input.attributes);
+
+container.append(input);
+
+
+
+const input = document.createElement("input");
+// input.type = "text";
+// input.placeholder = "please, input the text";
+input.setAttribute("type", "number");
+console.log(input);
+console.log(input.hasAttribute("placeholder"));
+
+console.log(input.getAttribute("type"));
+
+// input.removeAttribute("placeholder");
+
+console.log(input.attributes);
+
+const container = document.querySelector("[data-container]");
+container.append(input);
+const content = "Hello world!";
+
+const html = <h3 class="title"> ${content} </h3>;
+container.innerHTML = html;
+
+// container.append(input);
+const colorPickerOptions = [
+  { label: "red", color: "  #F44336" },
+  { label: "green", color: "  #4CAF58" },
+  { label: "blue", color: "  #2196F3" },
+  { label: "grey", color: "  #607D88" },
+  { label: "pink", color: "  #E91E63" },
+  { label: "indigo", color: "  #3F51B5" },
+];
+
+const container = document.querySelector("[data-container]");
+const html = colorPickerOptions
+  .map(
+    (button) =>
+      <button style="color:${button.color}">${button.label}</button>,
+  )
+  .join("");
+console.log(html);
+container.innerHTML = html;
+
+
+
+
+
+
+
+
+
+
+
